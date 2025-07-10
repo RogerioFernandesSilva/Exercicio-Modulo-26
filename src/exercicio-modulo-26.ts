77e55487fe7e055854fbc6a1c6c668f09df18b1a
Rogério Fernandes <rog.fernandes@yahoo.com.br>
2025-07-10 10:25:08 -0300

    Primeiro Comit

---
+++ src/exercicio-modulo-26.ts
@@ -0,0 +1,17 @@
+function multiply(a: number, b: number): number {
+  return a * b;
+}
+
+function greet(name: string): string {
+  return `Olá ${name}`;
+}
+
+// Exemplos de utilização da função multiply
+console.log(multiply(2, 3));     // Saída: 6
+console.log(multiply(5, 4));     // Saída: 20
+console.log(multiply(-2, 6));    // Saída: -12
+
+// Exemplos de utilização da função greet
+console.log(greet("Aline"));     // Saída: Olá Aline
+console.log(greet("Rafael"));       // Saída: Olá Rafael
+console.log(greet("Carolina"));     // Saída: Olá Carolina
